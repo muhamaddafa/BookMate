d2d1aaaccf57ae2b2891d963bedb87659696815d
KingAbby <abiandreatea80@gmail.com>
2024-10-25 16:07:26 +0700

    fix: design

---
+++ app/src/main/java/com/sumberrejeki/bookmate/BookScreenActivity.kt
@@ -0,0 +1,16 @@
+package com.sumberrejeki.bookmate
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class BookScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_book_screen)
+
+    }
+}
